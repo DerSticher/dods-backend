53f8358471e078e5bb2e9af02e8300b09b915a35
Richard Gottschalk <Gttschalk.Richard+github@gmail.com>
2016-12-26 16:32:43 +0100

    improved name parsing

--- application/src/main/java/io/dods/parser/ParseNameService.java
+++ application/src/main/java/io/dods/parser/ParseNameService.java
@@ -4,17 +4,27 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Richard Gottschalk
  */
 @Service
 class ParseNameService {
 
+    private static final Pattern PATTERN = Pattern.compile("([\\wäöüÄÖÜß]+) I-[IVX]+");
+
     public String parseName(Document document) {
         Elements elements = document.getElementsByTag("h1");
 
         if (elements.size() > 0) {
-            return elements.get(0).text();
+            String text = elements.get(0).text();
+            Matcher matcher = PATTERN.matcher(text);
+            if (matcher.find()) {
+                return matcher.group(1);
+            }
+            return text;
         }
 
         return "";
