c589a99268bb192bb50cb954ec3e67b5596283cc
Richard Gottschalk <Gttschalk.Richard+github@gmail.com>
2016-12-26 16:32:15 +0100

    replaced parser response code with 200

--- application/src/main/java/io/dods/api/parser/ParserApi.java
+++ application/src/main/java/io/dods/api/parser/ParserApi.java
@@ -22,25 +22,25 @@ public class ParserApi {
     @Autowired
     private ParseListService parseListService;
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "liturgie", method = RequestMethod.POST)
     public List<Liturgie> parse() {
         return parseListService.parseLiturgie("http://www.ulisses-regelwiki.de/index.php/lt_liturgien.html");
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "ritual", method = RequestMethod.POST)
     public List<Ritual> parseRitual() {
         return parseListService.parseRitual("http://www.ulisses-regelwiki.de/index.php/za_rituale.html");
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "segen", method = RequestMethod.POST)
     public List<Segen> parseSegen() {
         return parseListService.parseSegen("http://www.ulisses-regelwiki.de/index.php/Lit_Segnungen.html");
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "vorteil", method = RequestMethod.POST)
     public List<Vorteil> parseVorteil() {
         List<Vorteil> vorteil = new ArrayList<>();
@@ -49,19 +49,19 @@ public class ParserApi {
         return vorteil;
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "zauber", method = RequestMethod.POST)
     public List<Zauber> parseZauber() {
         return parseListService.parseZauber("http://www.ulisses-regelwiki.de/index.php/za_zaubersprueche.html");
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "zaubertrick", method = RequestMethod.POST)
     public List<Zaubertrick> parseZaubertrick() {
         return parseListService.parseZaubertrick("http://www.ulisses-regelwiki.de/index.php/Zauber_Zaubertricks.html");
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "zeremonie", method = RequestMethod.POST)
     public List<Zeremonie> parseZeremonie() {
         return parseListService.parseZeremonie("http://www.ulisses-regelwiki.de/index.php/lt_zeremonien.html");
